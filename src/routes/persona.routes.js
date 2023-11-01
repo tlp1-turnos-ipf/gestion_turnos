f9b577f0e62edc933ff956608bee1d131d910b73
Carlos Ezequiel Romero <ezequielromero3039@gmail.com>
2023-11-01 11:49:46 -0300

    feat: creando la carpeta del servidor

--- src/routes/persona.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-import { crearPersona } from "../controllers/persona.controller.js";
-
-const personaRouter = Router();
-
-personaRouter.post("/personas", crearPersona);
-
-export {personaRouter}
--- src/routes/user.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { crearUsuario } from "../controllers/usuario.controller.js";
-
-const userRouter = Router();
-
-//Route create User
-userRouter.post("/usuario", crearUsuario)
-
-export {userRouter}
